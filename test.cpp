debefab6f723f693d639b9774211a99a2c3f37e9
danielshih <dog830228@gmail.com>
2023-10-12 22:29:18 +0800

    modify code.

--- test.cpp
+++ test.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <cstdlib>
 #include "checker_postgres_server.h"
+bool IsEnvOk(const char* name, const char* val);
+
 
 int main(int argc, char * const argv[])
 {
+    const char* slotname = std::getenv("SlotName");
+    const char* pubname = std::getenv("PubName");
+    if (!IsEnvOk("SlotName", slotname) || !IsEnvOk("PubName", pubname))
+    {
+        return -1;
+    }
+
     auto param = parseParameter(argc, argv);
     PostgresServer server(argc, argv);
     server.identifySystem();
     server.setSlotandStartReplication("sub", "pub");
     return 0;
+}
+
+bool IsEnvOk(const char* name, const char* val) {
+    if (val != nullptr) {
+        std::cout << "Value of " << name << " is: " << val << std::endl;
+        return 1;
+    }
+    else {
+        std::cout << name <<" is not set." << std::endl;
+        return 0;
+    }
 }
